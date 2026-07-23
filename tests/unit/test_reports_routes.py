e764bddbaed1752e68378ddc21ec6f99a4044f6d
lucio richard <luciorichard08@gmail.com>
2026-07-23 12:25:28 +0800

    edited the report routes test

--- tests/unit/test_reports_routes.py
+++ tests/unit/test_reports_routes.py
@@ -9,32 +9,36 @@ def app():
     app.register_blueprint(reports_bp)
     return app
 
-def test_validate_report_payload_valid():
-    reason, description, error = _validate_report_payload({
-        'reason': 'Spam',
-        'description': 'This is spam'
-    })
-    assert reason == 'Spam'
-    assert description == 'This is spam'
-    assert error is None
+def test_validate_report_payload_valid(app):
+    with app.app_context():
+        reason, description, error = _validate_report_payload({
+            'reason': 'Spam',
+            'description': 'This is spam'
+        })
+        assert reason == 'Spam'
+        assert description == 'This is spam'
+        assert error is None
 
-def test_validate_report_payload_missing_reason():
-    _, _, error = _validate_report_payload({'description': 'test'})
-    assert error[1] == 400
-    assert error[0].json['error'] == 'reason is required'
+def test_validate_report_payload_missing_reason(app):
+    with app.app_context():
+        _, _, error = _validate_report_payload({'description': 'test'})
+        assert error[1] == 400
+        assert error[0].json['error'] == 'reason is required'
 
-def test_validate_report_payload_missing_description():
-    _, _, error = _validate_report_payload({'reason': 'Spam'})
-    assert error[1] == 400
-    assert error[0].json['error'] == 'description is required'
+def test_validate_report_payload_missing_description(app):
+    with app.app_context():
+        _, _, error = _validate_report_payload({'reason': 'Spam'})
+        assert error[1] == 400
+        assert error[0].json['error'] == 'description is required'
 
-def test_validate_report_payload_invalid_reason():
-    _, _, error = _validate_report_payload({
-        'reason': 'Invalid',
-        'description': 'test'
-    })
-    assert error[1] == 400
-    assert 'reason must be one of' in error[0].json['error']
+def test_validate_report_payload_invalid_reason(app):
+    with app.app_context():
+        _, _, error = _validate_report_payload({
+            'reason': 'Invalid',
+            'description': 'test'
+        })
+        assert error[1] == 400
+        assert 'reason must be one of' in error[0].json['error']
 
 def test_create_listing_report_unauthenticated(app):
     with app.test_client() as client:
